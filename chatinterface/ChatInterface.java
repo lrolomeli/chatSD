bdc56e4784533f036b7d21ec9438526ffcb98256
LuisRLP <lro.lomeli@gmail.com>
2024-03-26 00:11:16 -0600

    able to store messages linked to conversations

--- chatinterface/ChatInterface.java
+++ chatinterface/ChatInterface.java
@@ -7,7 +7,7 @@ public interface ChatInterface extends Remote {
 	// esta funcion consulta que la informacion de la contrasena ingresada
 	// por el usuario concuerde con la registrada previamente y coloca la
 	// bandera de estatus en online si la sesion fue iniciada con exito
-    boolean authenticateUser(String user, String password) throws RemoteException;
+    int authenticateUser(String user, String password) throws RemoteException;
     
     // recibe los parametros de nombre de usuario y contrasena y los registra
     // en la base de datos como un nuevo usuario
@@ -15,7 +15,7 @@ public interface ChatInterface extends Remote {
     
     // Esta funcion coloca nuevas entradas en la tabla de de mensajes
     // registrando el mensaje el emisor y el chat
-    boolean sendMessage(String message, String sender, int chat_id) throws RemoteException;
+    boolean sendMessage(String message, int user_id, int chat_id) throws RemoteException;
     
     // Retorna los ultimos 10 mensajes de la conversacion
     String loadMess(int chat_id) throws RemoteException;
@@ -28,13 +28,13 @@ public interface ChatInterface extends Remote {
     // Si la conversacion no existe, se va a crear una
     // nueva entrada en la base de datos y retornara el numero
     // de chat
-    int openChat(String user, String chat) throws RemoteException;
+    int openChat(int user_id, int chat) throws RemoteException;
     
     // Esta funcion hara una consulta de todos los usuarios registrados en 
     // la tabla de usuarios y devolvera el status [des]conectado de cada uno
     String userStatus() throws RemoteException;
     
     // Esta funcion modifica el status del usuario en la base de datos
-    boolean goOffline(String user) throws RemoteException;
+    boolean changeStatus(int user_id, boolean status) throws RemoteException;
     
 }
